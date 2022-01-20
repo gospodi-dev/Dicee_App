b604f9f93079300998b64b418949a648e4c39072
Gospodi <gospodijazzet@gmail.com>
2022-01-20 19:47:36 +0300

    Initial Commit

---
+++ Dicee App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee App
+//
+//  Created by Gospodi on 20.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
