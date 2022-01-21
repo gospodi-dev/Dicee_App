408322a6c6ecc2c8e8c01102ba1933eb22359d4c
Gospodi <gospodijazzet@gmail.com>
2022-01-21 09:38:02 +0300

    добавлены новые изображения при загрузке приложения, добавлен альфа канал

--- Dicee App/ViewController.swift
+++ Dicee App/ViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageView1.alpha = 0.5
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceFour")
+        diceImageView2.alpha = 0.5
         
     }
 
