d6ed380cf8fef0490554d2fb292ec51899b13d56
Gospodi <gospodijazzet@gmail.com>
2022-01-21 13:06:06 +0300

    добавлена рандомизация смены игральных костей

--- Dicee App/ViewController.swift
+++ Dicee App/ViewController.swift
@@ -8,21 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
-        diceImageView1.alpha = 0.5
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceFour")
-        diceImageView2.alpha = 0.5
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        //  let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
         
     }
-
-
+    
 }
-
